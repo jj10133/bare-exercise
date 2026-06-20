f9f81efb3ef5b0dddc15572eaf1a755d787dc792
JJ <warmachinesocial@skiff.com>
2026-06-19 20:48:11 -0500

    Fix - slight change in example
    as per readme for exercise small change was needed due to malformed
    request

--- examples/cloudflare.mjs
+++ examples/cloudflare.mjs
@@ -1,4 +1,4 @@
 import tcpCat from '../index.js'
 
-const response = await tcpCat('1.1.1.1', 80, 'GET / HTTP/1.1\r\n\r\nHost: cloudflare.com\r\n')
+const response = await tcpCat('1.1.1.1', 80, 'GET / HTTP/1.1\r\nHost: cloudflare.com\r\nConnection: close\r\n\r\n')
 console.log(response.toString())
